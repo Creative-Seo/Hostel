e0a05d46f310cdee03f42d5364cdcc68da424493
ilcomp <cgde88@gmail.com>
2015-09-29 14:35:44 +0300

    u

--- prod/en/about/content.php
+++ prod/en/about/content.php
@@ -2,20 +2,20 @@
 Located in the historic centre of Vladimir, Khrapovitskiy Hostel stand out against other 
 low-cost hostels for its cosy accommodations and high service. It is situated a brief walk 
 from a railway station and main tourist attractions.
-	<p></p>
+	</p><p>
 Our mission statement: to strike the balance between the affordability and comfortability. 
 For the very reason we’re proud to offer our guests the best price for the newly refurbished 
 home like accommodations designed in the contemporary manner. New furniture and 
 equipment clean and tidy.
-	<p></p>
+	</p><p>
 Designing our business we keenly examined the testimonials and did our best to provide you high 
 service in keeping with the world hostels’ traditions.
-	<p></p>
+	</p><p>
 Our friendly and knowledgeable staff helps you to settle any issue. Our hostel has a 24 hours check in 
 with the front desk personal available round the clock. We’d appreciate serving you in booking 
 tickets transport and restaurants. We’ll advice where to come in Vladimir to have fun and how to 
 get to main historic sites in the shortest way. We’ll help you to get into the spirit of Old Vladimir
-	<p></p>
+	</p><p>
 We’re no smoking hostel. Drinking alcohol is forbidden. With extra-departmental guards and day 
 and night surveillance you’ll feel quiet and safe. Lockers are available. Nothing will disturb you 
 after eventful day. Warm atmosphere and friendly staff will ensure you enjoy your stay with us. 
